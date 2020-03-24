97971368e021dc6820135679dfd70830c8905df8
Eloagbawe <eloagbawe@gmail.com>
2020-03-24 09:15:49 +0100

    Javascript task

---
+++ javascript_task_1.js
@@ -0,0 +1,11 @@
+var name = "Elo Agbawe";
+    var courses = ["HTML","Css","Javascript","NodeJs"]
+
+    console.log(name);
+    console.log(courses);
+    
+    for (let i=1; i <= 200; i++){
+        if ((i % 2) === 0) {
+            console.log(i);
+        }
+    }
